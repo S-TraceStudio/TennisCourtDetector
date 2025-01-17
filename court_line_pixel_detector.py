dff777ee94cb44967f057856feca68e067cb6328
Franck Chateau <franck@s-trace.com>
2025-01-17 21:36:10 +0100

    detecting

--- court_line_pixel_detector.py
+++ court_line_pixel_detector.py
@@ -51,7 +51,7 @@ class CourtLinePixelDetector:
 
         return luminanceChannel
 
-    def detect_line_pixels(self, image):
+    def detect_line_pixels(self, image, debug=False):
         # Assuming this class is defined elsewhere
         bg_value = global_params.bg_value
         pixel_image = np.full((image.shape[0], image.shape[1]), bg_value, dtype=np.uint8)
@@ -61,6 +61,9 @@ class CourtLinePixelDetector:
             for y in range(image.shape[0]):
                 pixel_image[y, x] = self.is_line_pixel(image, x, y)
 
+        if debug:
+            displayDebugImage(pixel_image)
+
         return pixel_image
 
     def filter_line_pixels(self, binary_image, luminance_image):
@@ -69,7 +72,39 @@ class CourtLinePixelDetector:
         return filtered_image
 
     def is_line_pixel(self, image, x, y):
-        return image[y, x] > self.parameters.threshold
+        if x < self.parameters.t or image.shape[1] - x <= self.parameters.t:
+            return global_params.bg_value
+        if y < self.parameters.t or image.shape[0] - y <= self.parameters.t:
+            return global_params.bg_value
+
+        value = image[y, x]
+
+        if value < self.parameters.threshold:
+            return global_params.bg_value
+
+        # Use np.clip to constrain the values
+        max_value = np.iinfo(value.dtype).max
+        min_value = np.iinfo(value.dtype).min
+
+        top_value = image[y - self.parameters.t, x]
+        lower_value = image[y + self.parameters.t, x]
+        left_value = image[y, x - self.parameters.t]
+        right_value = image[y, x + self.parameters.t]
+
+        value = np.clip(value, min_value, max_value)
+        left_value = np.clip(left_value, min_value, max_value)
+        right_value = np.clip(right_value, min_value, max_value)
+        top_value = np.clip(top_value, min_value, max_value)
+        lower_value = np.clip(lower_value, min_value, max_value)
+
+        if (value - left_value > self.parameters.diffThreshold) and (value - right_value > self.parameters.diffThreshold):
+            return global_params.fg_value
+
+        if (value - top_value > self.parameters.diffThreshold) and (value - lower_value > self.parameters.diffThreshold):
+            return global_params.fg_value
+        return global_params.bg_value;
+
+
 
     def compute_structure_tensor_elements(self, image):
         dx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=self.parameters.gradientKernelSize)
