638833693ac67ae3801d97aae98c89f318733bc6
Franck Chateau <franck@s-trace.com>
2025-02-01 22:15:48 +0100

    adding debug window name

--- utils.py
+++ utils.py
@@ -90,9 +90,9 @@ def replace_file_extension(filename, extension):
     filename_replace_ext = new_filename.with_suffix(extension)
     return filename_replace_ext
 
-def displayDebugImage(debugImage,scale=1):
+def displayDebugImage(debugImage,scale=1,widow_name='Debug Image'):
     scaled_image = cv2.resize(debugImage, None, fx=scale, fy=scale,interpolation=cv2.INTER_LINEAR)
-    cv2.imshow('Debug Image', scaled_image)
+    cv2.imshow(widow_name, scaled_image)
     cv2.waitKey(0)
     #cv2.destroyWindow('Debug Image')
     cv2.destroyAllWindows()
