8d67905e39b3b3bba54d8a5e528617f6ebdc74c9
Franck Chateau <franck@s-trace.com>
2025-02-02 22:17:42 +0100

    adding tennis court fitter

---
+++ tennis_court_fitter.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+from line import Line
+from tennis_court_model import TennisCourtModel
+from utils import displayDebugImage
+from debug_helper import draw_lines
+
+class TennisCourtFitter:
+    debug = False
+    windowName = "TennisCourtFitter"
+
+    def __init__(self):
+        debug = True
+
+    def run(self, lines, binaryImage, rgbImage):
+        hLines, vLines = self.get_horizontal_and_vertical_lines(lines, rgbImage)
+        self.sortHorizontalLines(hLines, rgbImage)
+        self.sortVerticalLines(vLines, rgbImage)
+
+
+
+
+    def get_horizontal_and_vertical_lines(self, lines, rgbImage):
+        hLines = []
+        vLines = []
+        for line in lines:
+            if line.is_vertical(line):
+                vLines.append(line)
+            else:
+                hLines.append(line)
+
+        if self.debug:
+            print(f"Horizontal lines = {len(hLines)}")
+            print(f"Vertical lines = {len(vLines)}")
+            image = rgbImage.copy()
+            draw_lines(hLines, image, color=(255, 0, 0))
+            draw_lines(vLines, image, color=(0, 255, 0))
+            displayDebugImage(image, self.windowName )
+
+        return hLines, vLines
+
+
+
+
+
+
+
+
+
+
+
